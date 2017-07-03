562d89c362142125c22e0e88f7024e0b003df5b8
Zudo <ZudoMC@users.noreply.github.com>
2017-07-03 20:47:21 +0100

    Update hgs.js

--- hgs.js
+++ hgs.js
@@ -119,7 +119,7 @@ function randomTributes(n) {
 
 function getEvent(fatal)  {
     let list = gameSettings.eventTypes[fatal ? "fatal" : "nonFatal"];
-    list = list.filter(e => e.count >=  livingTributes().length);
+    list = list.filter(e => e.count <= livingTributes().length);
     return list[Math.floor(Math.random()*list.length)];
 }
 
